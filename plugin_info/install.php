e075b21979dd594a351d32af73f51d77be30d2b4
ktn <jeedom@7ea6d355132c>
2023-03-09 00:05:11 +0100

    activation du panel à l'installation

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -32,6 +32,10 @@ function devolo_cpl_checkMac() {
 	}
 }
 
+function devolo_cpl_goto_7() {
+	config::save('displayDesktopPanel,'1',devolo_cpl);
+}
+
 function devolo_cpl_goto_6() {
 	foreach (devolo_cpl::byType('devolo_cpl') as $eqLogic){
 		$eqLogic->sortCmds();
@@ -66,7 +70,7 @@ function devolo_upgrade_to_level($level) {
 function devolo_cpl_upgrade() {
 	$pluginLevel = config::byKey('pluginLevel','devolo_cpl',0);
 	log::add("devolo_cpl","info","pluginLevel: " . $pluginLevel);
-	for ($level = 1; $level <= 6; $level++) {
+	for ($level = 1; $level <= 7; $level++) {
 		if ($pluginLevel < $level) {
 			devolo_upgrade_to_level($level);
 			config::save('pluginLevel',$level,'devolo_cpl');
