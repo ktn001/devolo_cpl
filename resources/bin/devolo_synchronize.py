d6e0efd8ffb85bcaa6d068e739f1ee20de360e4b
JeedomBot lint Pretty <commit@jeedom.com>
2024-09-04 12:33:31 +0000

    Fix code style issues with Black

--- resources/bin/devolo_synchronize.py
+++ resources/bin/devolo_synchronize.py
@@ -16,27 +16,29 @@ import devolo_plc_api
 import devolo_plc_api.network
 from devolo_plc_api import Device
 
-action = ''
+action = ""
 
-#===============================================================================
+
+# ===============================================================================
 # exit_handler
-#...............................................................................
+# ...............................................................................
 # function appelée automatiquement à la fermeture du programme
-#===============================================================================
+# ===============================================================================
 def exit_handler():
-    info ("===================== END =====================")
+    info("===================== END =====================")
 
-#===============================================================================
+
+# ===============================================================================
 # options
-#...............................................................................
+# ...............................................................................
 # Traitement des options globales de la ligne de commande. Les options
 # spécifiques des commande seront traitées dans un deuxième temps
-#===============================================================================
+# ===============================================================================
 def options():
     global action
 
     parser = argparse.ArgumentParser(allow_abbrev=False)
-    parser.add_argument('-l', '--loglevel')
+    parser.add_argument("-l", "--loglevel")
     args = parser.parse_known_args()
 
     if args[0].loglevel:
@@ -46,44 +48,50 @@ def options():
 
     return args[1]
 
-#===============================================================================
+
+# ===============================================================================
 # device_definition
-#...............................................................................
+# ...............................................................................
 # Retourne les infos nécessaires à la création d'un eqLogic
-#===============================================================================
+# ===============================================================================
 def device_definition(device):
     device.connect()
-    print (device)
-    print (device.product)
+    print(device)
+    print(device.product)
     device.disconnect()
 
-#===============================================================================
+
+# ===============================================================================
 # getDeviceInfos
-#...............................................................................
+# ...............................................................................
 # Retourne les infos d'un device
-#===============================================================================
+# ===============================================================================
 async def getDeviceInfos(device, names):
     ret = {}
     await device.async_connect()
     serial = device.serial_number
     try:
         for i in range(20):
-            if not hasattr(device.plcnet,'async_get_network_overview'):
-                logging.info(f"{serial}: pas d'attribut 'async_get_network_oveview' ==> reconnection {i+1}")
+            if not hasattr(device.plcnet, "async_get_network_overview"):
+                logging.info(
+                    f"{serial}: pas d'attribut 'async_get_network_oveview' ==> reconnection {i+1}"
+                )
                 await device.async_disconnect()
                 await device.async_connect()
             else:
                 break
-        ret['serial'] = serial
-        ret['model'] = device.product
-        ret['mac'] = device.mac
-        ret['ip'] = device.ip
+        ret["serial"] = serial
+        ret["model"] = device.product
+        ret["mac"] = device.mac
+        ret["ip"] = device.ip
         network = await device.plcnet.async_get_network_overview()
-        for i in range (0, len(network.devices)):
+        for i in range(0, len(network.devices)):
             dev = network.devices[i]
             if dev.mac_address in names:
                 if names[dev.mac_address] != dev.user_device_name:
-                    warning(f"Deux noms différents pour la mac address {dev.mac_address}: {names[dev.mac_address]} et {dev.user_device_name}")
+                    warning(
+                        f"Deux noms différents pour la mac address {dev.mac_address}: {names[dev.mac_address]} et {dev.user_device_name}"
+                    )
             else:
                 names[dev.mac_address] = dev.user_device_name
     except AttributeError as e:
@@ -91,15 +99,16 @@ async def getDeviceInfos(device, names):
     await device.async_disconnect()
     return ret
 
-#===============================================================================
+
+# ===============================================================================
 # syncDevolo
-#...............................................................................
+# ...............................................................................
 # Recupère les informations des équipement Devolo pour synchonisation dans
 # Jeedom
-#===============================================================================
+# ===============================================================================
 async def syncDevolo():
     devices = await devolo_plc_api.network.async_discover_network()
-    logging.info (f"{len(devices)} devices found.")
+    logging.info(f"{len(devices)} devices found.")
     time.sleep(5)
     result = {}
     names = {}
@@ -110,10 +119,10 @@ async def syncDevolo():
         for serial in devices:
             result[serial] = await tasks[serial]
         for serial in result:
-            if result[serial]['mac'] in names:
-                result[serial]['name'] = names[result[serial]['mac']]
-        print (json.dumps(result))
-            
+            if result[serial]["mac"] in names:
+                result[serial]["name"] = names[result[serial]["mac"]]
+        print(json.dumps(result))
+
     except Exception as e:
         logging.error("======================================")
         logging.error("Error discovering devices: " + str(e))
@@ -125,16 +134,17 @@ async def syncDevolo():
         logging.error(traceback.format_exc())
         logging.error("======================================")
 
-  ###########################
- #                           #
+
+###########################
+#                           #
 #  #    #    ##    #  #    #  #
 #  ##  ##   #  #   #  ##   #  #
 #  # ## #  #    #  #  # #  #  #
 #  #    #  ######  #  #  # #  #
 #  #    #  #    #  #  #   ##  #
 #  #    #  #    #  #  #    #  #
- #                           #
-  ###########################
+#                           #
+###########################
 
 atexit.register(exit_handler)
 args = options()
